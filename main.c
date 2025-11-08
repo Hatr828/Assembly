ce0d8f8860dc1086e67508d5b68a9ad79157dd7c
Hatr828 <hatr828@gmail.com>
2025-11-07 20:25:45 -0600

    sum_to_n_asm

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int c_to_f_asm(int c);
-int abs_asm(int x);
+int c_to_f_asm(int num);
+int abs_asm(int num);
+long sum_to_n_formula_asm(long num);
 
 int main(void) {
-  int res = abs_asm(40);
+  long long a = 0;
+  int res = sum_to_n_formula_asm(10);
 
   printf("res: %d \n",res);
 
